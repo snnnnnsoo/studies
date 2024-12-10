10f7fd3986ab36eae00f1171d7e4b2cdb3d255e2
Alexander Kuka <lxndrkk6002@gmail.com>
2024-12-11 00:22:40 +0300

    step 2

--- menu/menu.c
+++ menu/menu.c
@@ -6,4 +6,25 @@ int main()
 {
     std::setlocale(LC_ALL, "");
     
+    int user_input;
+    do {
+        std::cout << "Читалка приветствует тебя, мой юный ученик!" << std::endl;
+        std::cout << "1 - Русские писатели";
+        std::cout << "0 - Завершить чтение";
+        std::cout << "Читалка > ";
+        
+        std::cin >> user_input;
+        if (user_input == 1)
+        {
+            
+        }
+        else if (user_input == 0)
+        {
+            exit(0);
+        }
+        
+        std::cout << std::endl;
+    } while (true);
+    
+    return 0;
 }
