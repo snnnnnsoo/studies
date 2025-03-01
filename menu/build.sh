cd6e34bd2fcb93a8217f72700103f1d9c9a85148
Alexander Kuka <lxndrkk6002@gmail.com>
2025-03-01 11:46:45 +0300

    иерархия

--- menu/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-clang++ -o3 menu.cpp menu_functions.cpp menu_items.cpp -o menu
-
-./menu
---
+++ projects/bubble sort/include.h/sortings_busko.hpp
@@ -0,0 +1,9 @@
+#ifndef SORTINGS_HPP
+#define SORTINGS_HPP
+
+    void Insert_sort(std::vector<int> &array_to_sort,int left_edge, int right_edge, int m);
+    void Merge(std::vector<int> &array_to_sort, int left_edge, int right_edge, int m, int median);
+    void Merge_sort(std::vector<int> &array_to_sort, int left_edge, int right_edge, int m);
+
+
+#endif
